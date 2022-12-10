2f6a0873e65f9795f46553b6e02ce0a4e2c70adc
Andrey Kumykov <aakumykov@yandex.ru>
2022-12-10 20:51:49 +0300

    Добавил в PlayerService метод для создания канала уведомлений.
    (cherry picked from commit a112832707c398221cb6d4190ed5a447930b4777)

--- player_service/src/main/java/com/example/player_service/PlayerService.java
+++ player_service/src/main/java/com/example/player_service/PlayerService.java
@@ -6,11 +6,14 @@ import android.content.Intent;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationChannelCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.android.exoplayer2.ExoPlayer;
 
 public class PlayerService extends Service {
 
+    private static final String CHANNEL_ID = "Player_service_notification_channel";
     private CustomPlayer mCustomPlayer;
 
     public static Intent getIntent(Context context) {
@@ -27,6 +30,7 @@ public class PlayerService extends Service {
         super.onCreate();
         final ExoPlayer exoPlayer = new ExoPlayer.Builder(this).build();
         mCustomPlayer = new CustomPlayer(exoPlayer);
+        prepareNotificationChannel();
     }
 
     @Override
@@ -42,4 +46,32 @@ public class PlayerService extends Service {
     public void stop() {
         stopSelf();
     }
+
+
+    private void prepareNotificationChannel() {
+        NotificationManagerCompat.from(this).createNotificationChannel(notificationChannel());
+    }
+
+    private NotificationChannelCompat notificationChannel() {
+        return new NotificationChannelCompat.Builder(CHANNEL_ID, NotificationManagerCompat.IMPORTANCE_LOW)
+                .setName(getString(R.string.PLAYER_SERVICE_notification_channel_name))
+                .build();
+    }
+
+    /*private void showPersistentNotification() {
+        startForeground(R.id.player_service_notification, notification());
+    }
+
+    private void hidePersistentNotification() {
+        stopForeground(true);
+    }*/
+
+    /*private Notification notification() {
+        return new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_service_notification)
+                .setContentTitle(getString(R.string.PLAYER_SERVICE_notification_title))
+                .setContentText(getString(R.string.PLAYER_SERVICE_notification_text))
+                .build();
+    }*/
+
 }
